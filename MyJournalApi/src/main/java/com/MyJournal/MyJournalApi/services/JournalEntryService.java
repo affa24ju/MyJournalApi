92d60b12ddcc8a1162f13a50e18e0443f05ec435
Fatema Afroza <fafroza2017@gmail.com>
2025-09-23 14:04:01 +0200

    Lagt till JournalStatsService

--- MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/services/JournalEntryService.java
+++ MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/services/JournalEntryService.java
@@ -38,7 +38,7 @@ public class JournalEntryService {
     }
 
     // Metod för att visa statistik för en tidperiod
-    public List<JournalEntry> getJournalEntriesByDateReange(User user, LocalDateTime startDate, LocalDateTime endDate) {
+    public List<JournalEntry> getJournalEntriesByDateRange(User user, LocalDateTime startDate, LocalDateTime endDate) {
         return journalEntryRepository.findByUserIdAndCreatedAtBetween(user.getId(), startDate, endDate);
     }
 
