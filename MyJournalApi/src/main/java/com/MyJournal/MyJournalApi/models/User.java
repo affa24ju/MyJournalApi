c5710112f9a82df83970d30ef9b592669db8861e
Fatema Afroza <fafroza2017@gmail.com>
2025-09-23 11:00:38 +0200

    Skapat user model

---
+++ MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/models/User.java
@@ -0,0 +1,21 @@
+package com.MyJournal.MyJournalApi.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    private String id;
+    private String username;
+    private String password;
+}
