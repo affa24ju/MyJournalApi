278ee0904e9253d538ed54ff32d3bc8514ac1822
Fatema Afroza <fafroza2017@gmail.com>
2025-09-23 12:37:25 +0200

    Lagt till createJournalEntry metod inne i service

--- MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/services/JournalEntryService.java
+++ MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/services/JournalEntryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.MyJournal.MyJournalApi.dtos.JournalEntryRequest;
 import com.MyJournal.MyJournalApi.models.JournalEntry;
 import com.MyJournal.MyJournalApi.repositories.JournalEntryRepository;
 
@@ -16,9 +17,14 @@ public class JournalEntryService {
     private final JournalEntryRepository journalEntryRepository;
 
     // Metod för att skapa en journalpost
-    public JournalEntry createJournalEntry() {
+    public JournalEntry createJournalEntry(JournalEntryRequest request) {
+        JournalEntry newEntry = JournalEntry.builder()
+                .note(request.getNote())
+                .feeling(request.getFeeling())
+                .createdAt(request.getCreatedAt())
+                .build();
 
-        return null; // Placeholder return
+        return journalEntryRepository.save(newEntry); 
 
     }
 
