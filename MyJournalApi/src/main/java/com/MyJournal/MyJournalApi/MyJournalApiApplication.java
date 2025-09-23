b92f7f09db61a632ad598dd4955c869fc7db38e9
Fatema Afroza <fafroza2017@gmail.com>
2025-09-23 10:25:43 +0200

    Lägger till git

---
+++ MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/MyJournalApiApplication.java
@@ -0,0 +1,13 @@
+package com.MyJournal.MyJournalApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJournalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJournalApiApplication.class, args);
+	}
+
+}
---
+++ MyJournalApi/src/test/java/com/MyJournal/MyJournalApi/MyJournalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyJournal.MyJournalApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJournalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
