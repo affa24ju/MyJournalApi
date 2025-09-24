7fabbded7112417bb986a112d25e573e35d9b406
Fatema Afroza <fafroza2017@gmail.com>
2025-09-24 08:42:31 +0200

    Lagt till generateToken() i JwtService

---
+++ MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/security/JwtService.java
@@ -0,0 +1,29 @@
+package com.MyJournal.MyJournalApi.security;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "2de812086b19ffb68ce7757586e69c26c7da886e2ed78bc52bd721b841638b9e";
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 timme
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+}
