649d4a48d37ca32ebc71e9c00ff86dfdce489d4f
Fatema Afroza <fafroza2017@gmail.com>
2025-09-24 08:03:28 +0200

    Skapat UserDetailsServiceImpl inne i service

--- MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/models/User.java
+++ MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/models/User.java
@@ -1,7 +1,12 @@
 package com.MyJournal.MyJournalApi.models;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,13 +14,41 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Document(collection = "users")
-@Data   // Generates getters, setters, toString, equals, and hashCode methods
+@Data // Generates getters, setters, toString, equals, and hashCode methods
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
     @Id
     private String id;
     private String username;
     private String password;
+
+    // Implement UserDetails methods
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Returnerar en tom lista eftersom vi inte har några roller eller behörigheter
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
