8a5eefacbcad247e0bc2498d268b7cf3d8925cd6
Fatema Afroza <fafroza2017@gmail.com>
2025-09-24 10:27:05 +0200

    La till authenticaitonManager

--- MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/security/SecurityConfig.java
+++ MyJournalApi/src/main/java/com/MyJournal/MyJournalApi/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.MyJournal.MyJournalApi.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,6 +29,12 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
